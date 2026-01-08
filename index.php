c20e50e140398415ee2adf91b11866208c360230
abihafatima1415-sys <abihafatima1415@gmail.com>
2026-01-08 23:20:51 +0500

    include css

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
     <!-- Page title and meta information -->
-    <meta charset="UTF-8"><link rel="stylesheet" href="css/site.css" />
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/site.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fitness Management System</title>
 
